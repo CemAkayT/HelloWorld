2a268e679f58116f5e1308b196e7200a3dff40d6
cemakay <cema0092@stud.kea.dk>
2021-09-16 14:18:37 +0200

    Det virker!!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Det virker skisme!");
+        System.out.println("det virker stadig");
     }
 }
